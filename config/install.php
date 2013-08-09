1834ab3d33525a35ae731f3868e5b8fdeabd8ddf
MGhayour <Me@MGhayour.ir>
2013-08-09 22:36:09 +0430

    read data (sample)

--- config/install.php
+++ config/install.php
@@ -8,11 +8,24 @@ $res = mysqli_query($db, $sql);
 check_mysqli_error($db,$res);
 
 // Write data :
+echo "<br />Write Data :<br />";
 $safestr=mysqli_real_escape_string($db," ' An 'Not Safe' String ! ' ");
 $sql = "INSERT INTO testTable (testField) VALUES ('".$safestr."')";
-echo($safestr);
 $res = mysqli_query($db, $sql);
 check_mysqli_error($db,$res);
+
+// Read data :
+echo "<br />Read Data :<br />";
+$sql = "SELECT * FROM testTable";
+$res = mysqli_query($db, $sql);
+check_mysqli_error($db,$res);
+echo "The Number of Rows is : ".mysqli_num_rows($res)."<br />";
+if ($res) {
+	while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+		echo "The ID is ".$row['id']." and the text is: ".$row['testField']."<br/>";
+	}
+}
+mysqli_free_result($res); //cleaning the result {For More RAM space !}
 
 
 
