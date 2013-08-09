103f5259c9ca8660de0f5ccf0c3ebc37dbbe341e
MGhayour <Me@MGhayour.ir>
2013-08-09 22:11:10 +0430

    Write data sample complete

--- config/install.php
+++ config/install.php
@@ -7,8 +7,13 @@ testField VARCHAR(75))";
 $res = mysqli_query($db, $sql);
 check_mysqli_error($db,$res);
 
-$sql = "INSERT INTO testTable (testField) VALUES ('some value')";
+// Write data :
+$safestr=mysqli_real_escape_string($db," ' An 'Not Safe' String ! ' ");
+$sql = "INSERT INTO testTable (testField) VALUES ('".$safestr."')";
+echo($safestr);
 $res = mysqli_query($db, $sql);
 check_mysqli_error($db,$res);
+
+
 
 ?>
