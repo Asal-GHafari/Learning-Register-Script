e37a67349063a3680be33327cbb8464374da8fb6
MGhayour <Me@MGhayour.ir>
2013-08-10 17:22:26 +0430

    read messeges added

--- chat.php
+++ chat.php
@@ -1,7 +1,7 @@
 <?php
+// init Script :
 include("config/config.php");
-
-$sql = "CREATE TABLE messege
+$sql = "CREATE TABLE messeges
 (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 sender VARCHAR(15),
@@ -11,6 +11,16 @@ messege VARCHAR(255)
 $res = mysqli_query($db, $sql);
 check_mysqli_error($db,$res);
 
+// Read messeges :
+echo "<br /><br /><br /><br /><h1>Messeges :</h1>";
+$sql = "SELECT * FROM messeges";
+$res = mysqli_query($db, $sql);
+check_mysqli_error($db,$res);
+if ($res) {
+	while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+		echo "'".$row['sender']."' Send a messege to '".$row['reciver']."' :	".$row['messege']."<br/>";
+	}
+}
 
 
 ?>
