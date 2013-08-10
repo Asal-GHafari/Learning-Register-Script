b0a444fa886f4db885a2336a8bd7a2b5d5fc533c
MGhayour <Me@MGhayour.ir>
2013-08-10 17:16:36 +0430

    solve a simple bug :D

--- chat.php
+++ chat.php
@@ -4,8 +4,8 @@ include("config/config.php");
 $sql = "CREATE TABLE messege
 (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-sender VARCHAR(15)
-reciver VARCHAR(15)
+sender VARCHAR(15),
+reciver VARCHAR(15),
 messege VARCHAR(255)
 )";
 $res = mysqli_query($db, $sql);
