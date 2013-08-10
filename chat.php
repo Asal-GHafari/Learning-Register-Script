43d90847d6552a1466605345d9f41228acd5f7d6
MGhayour <Me@MGhayour.ir>
2013-08-10 18:33:10 +0430

    Write Messege added

--- chat.php
+++ chat.php
@@ -11,8 +11,25 @@ messege VARCHAR(255)
 $res = mysqli_query($db, $sql);
 check_mysqli_error($db,$res);
 
+// Write Messege :
+echo "<br /><br /><h3>Write a messege :</h3>";
+$sender='';
+$reciver='';
+$messege='';
+ if (!empty($_POST))
+{
+	$sender=mysqli_real_escape_string($db,$_POST['sender']);
+	$reciver=mysqli_real_escape_string($db,$_POST['reciver']);
+	$messege=mysqli_real_escape_string($db,$_POST['messege']);
+	$sql = "INSERT INTO messeges (sender,reciver,messege) VALUES ('".$sender."','".$reciver."','".$messege."')";
+	echo $sql."<br />";
+	$res = mysqli_query($db, $sql);
+	check_mysqli_error($db,$res);
+	echo "<h3>Added ! :</h3>";
+}
+
 // Read messeges :
-echo "<br /><br /><br /><br /><h1>Messeges :</h1>";
+echo "<br /><h3>Messeges :</h3>";
 $sql = "SELECT * FROM messeges";
 $res = mysqli_query($db, $sql);
 check_mysqli_error($db,$res);
@@ -22,5 +39,15 @@ if ($res) {
 	}
 }
 
+
+?>
+<form action="" method="post" >
+	Sender:<input name="sender" type="text" value="<?php echo $sender ?>" size="12" />
+	Reciver:<input name="reciver" type="text" value="<?php echo $reciver ?>" size="12" /> <br />
+	Messege:<input name="messege" type="text" size="25" />
+	<input type="submit" value="Send" />
+</form>
+
+<?php
 
 ?>
