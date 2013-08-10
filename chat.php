d13f652b504637fe5a93d62c335468a2424b09da
MGhayour <Me@MGhayour.ir>
2013-08-10 17:14:33 +0430

    init a simple chat script

---
+++ chat.php
@@ -0,0 +1,16 @@
+<?php
+include("config/config.php");
+
+$sql = "CREATE TABLE messege
+(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+sender VARCHAR(15)
+reciver VARCHAR(15)
+messege VARCHAR(255)
+)";
+$res = mysqli_query($db, $sql);
+check_mysqli_error($db,$res);
+
+
+
+?>
