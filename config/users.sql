0e5e900a37432321c1bdbc1617a673f89c9a1a1c
MGhayour <Me@MGhayour.ir>
2013-08-13 22:01:07 +0430

    some wrong code fixed !

--- config/users.sql
+++ config/users.sql
@@ -1,11 +1,11 @@
 -- 
--- Table structure for table 'users'
+-- Table structure for table `users`
 -- 
-CREATE TABLE 'users' ('id' INT NOT NULL AUTO_INCREMENT,
-'username' varchar(25) NOT NULL ,
-'password' varchar(25) NOT NULL ,
-'email' varchar(35) NOT NULL ,
-'avatar' text ,
-'signup_date' int(10) NOT NULL ,
-PRIMARY KEY ('id')
+CREATE TABLE `users` (`id` INT NOT NULL AUTO_INCREMENT,
+`username` varchar(25) NOT NULL ,
+`password` varchar(25) NOT NULL ,
+`email` varchar(35) NOT NULL ,
+`avatar` text ,
+`signup_date` int(10) NOT NULL ,
+PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8
